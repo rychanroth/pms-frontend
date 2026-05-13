6bb4475fbe92c839e07f2699442d002bd601636a
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-13 19:05:46 +0700

    feat(service): on `DjangoApiService` add `login`

---
+++ app/Services/DjangoApiService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class DjangoApiService
+{
+    protected $baseUrl;
+
+    public function __construct()
+    {
+        // Change this to match your Django local server URL!
+        $this->baseUrl = 'http://127.0.0.1:8000/api';
+    }
+
+    public function login($username, $password)
+    {
+        $response = Http::post("{$this->baseUrl}/login/", [
+            'username' => $username,
+            'password' => $password,
+        ]);
+
+        return $response;
+    }
+}
