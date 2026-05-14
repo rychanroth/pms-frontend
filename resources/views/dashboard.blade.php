a0af66dfef71a4664fef0a56deba70829ad8e7a2
Ry Chanroth <rychanroth2009@gmail.com>
2026-05-14 17:20:38 +0700

    feat(view): change text to display django User info

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,8 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <p>Token: {{ session('api_token') }}</p>
+                    <p>Role: {{ session('user_role') }}</p>
                 </div>
             </div>
         </div>
